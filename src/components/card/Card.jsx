5250557d48d053e3afe12d84fe8e691d6925c2c4
Alexander-Agu <theonlyagu@gmail.com>
2025-04-21 15:57:40 +0200

    Made the card component dynamic

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import "./card.css"
 
-function Card() {
+function Card({title, content}) {
   return (
     <div className="card">
         <p className='cardTopic'>
-            Purpose of the Platform
+            {title}
         </p>
 
         <p className='cardContent'>
-            This platform is built to support learners in mastering AWS Cloud Computing through an engaging, quiz-based learning experience. Whether you're new to cloud technologies or preparing for AWS certification, the platform provides an effective way to reinforce your knowledge and track your progress.
+            {content}
         </p>
     </div>
   )
