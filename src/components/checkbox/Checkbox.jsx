8774d37d578c82a26001853e4363f94715b48469
Alexander-Agu <theonlyagu@gmail.com>
2025-04-24 16:23:12 +0200

    Updated the checkboc component

---
+++ src/components/checkbox/Checkbox.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import "./checkbox.css"
+
+function Checkbox({a, b, c, d, setAnswer, data}) {
+
+    function selectOnlyThis(checkbox) {
+        const checkboxes = document.getElementsByName('option');
+        checkboxes.forEach((item) => {
+            if (item !== checkbox) item.checked = false;
+        });
+
+        setAnswer(checkbox.value)
+    }
+  return (
+    <>
+        <form className='checkboxForm'>
+            <label>
+                <input type="checkbox" name="option" value={data[1]} onClick={(e) => { selectOnlyThis(e.target); }} /> {a}
+            </label>
+
+            <label>
+                <input type="checkbox" name="option" value={data[2]} onClick={(e) => { selectOnlyThis(e.target); }} /> {b}
+            </label>
+
+            <label>
+                <input type="checkbox" name="option" value={data[3]} onClick={(e) => { selectOnlyThis(e.target); }} /> {c}
+            </label>
+
+            <label>
+                <input type="checkbox" name="option" value={data[4]} onClick={(e) => { selectOnlyThis(e.target); }} /> {d}
+            </label>
+        </form>
+    </>
+  )
+}
+
+export default Checkbox
